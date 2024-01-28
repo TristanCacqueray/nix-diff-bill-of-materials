346229ce73c4b2c555f48515a57e4a3ca333fdd6
Tristan Cacqueray <tdecacqu@redhat.com>
2024-01-28 07:29:32 -0500

    Add documentation

--- Main.hs
+++ Main.hs
@@ -1,6 +1,11 @@
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE NoFieldSelectors #-}
-
+-- |
+-- Module      :  Main
+-- Copyright   :  Tristan Cacqueray
+-- Maintainer  :  tdecacqu@redhat.com
+--
+-- SPDX-License-Identifier: MIT
+--
+-- Main entry point for the nix-diff-bill-of-materials application.
 module Main where
 
 import Data.Attoparsec.Text.Lazy qualified
@@ -15,18 +20,38 @@ import Options.Applicative qualified as O
 import RIO
 import Turtle qualified
 
+-------------------------------------------------------------------------------
+-- Low level derivation parser: 'DrvInfo'
+-------------------------------------------------------------------------------
+
+-- | Raw nix derivations are parsed into a 'DrvInfo'.
 data DrvInfo
-    = MaterialInfo
+    = -- | This is a package:
+      MaterialInfo
         { name :: Text
         , version :: Text
         , src :: FilePath
         }
-    | Source
+    | -- | This is a source fetcher:
+      Source
         { out :: FilePath
         , url :: Text
         }
 
-type Drv = Nix.Derivation.Derivation FilePath Text
+parseMaterial :: Drv -> Maybe DrvInfo
+parseMaterial drv = do
+    name <- Map.lookup "pname" drv.env
+    version <- Map.lookup "version" drv.env
+    src <- Text.unpack <$> Map.lookup "src" drv.env
+    pure MaterialInfo{name, src, version}
+
+parseSource :: Drv -> Maybe DrvInfo
+parseSource drv = do
+    -- Question: why some (most?) urls are not using the builtin:fetch?
+    -- This hack seems necessary to detect fetchFromGitHub usage.
+    out <- Text.unpack <$> Map.lookup "out" drv.env
+    url <- Map.lookup "urls" drv.env
+    pure Source{out, url}
 
 -- rust crate from crane does not use pname/version
 parseRustMaterial :: Drv -> Maybe DrvInfo
@@ -51,22 +76,23 @@ parseRustMaterial drv = do
             src : _ -> pure $ Text.unpack src
             _ -> Nothing
 
-parseMaterial :: Drv -> Maybe DrvInfo
-parseMaterial drv =
-    parseRustMaterial drv <|> do
-        name <- Map.lookup "pname" drv.env
-        version <- Map.lookup "version" drv.env
-        src <- Text.unpack <$> Map.lookup "src" drv.env
-        pure MaterialInfo{name, src, version}
+parseDrvInfo :: Drv -> Maybe DrvInfo
+parseDrvInfo drv = parseMaterial drv <|> parseSource drv <|> parseRustMaterial drv
 
-parseSource :: Drv -> Maybe DrvInfo
-parseSource drv = do
-    -- Question: why some (most?) urls are not using the builtin:fetch?
-    -- This hack seems necessary to detect fetchFromGitHub usage.
-    out <- Text.unpack <$> Map.lookup "out" drv.env
-    url <- Map.lookup "urls" drv.env
-    pure Source{out, url}
+type Drv = Nix.Derivation.Derivation FilePath Text
 
+readDrv :: FilePath -> IO Drv
+readDrv fp = do
+    text <- Data.Text.Lazy.IO.readFile fp
+    case Data.Attoparsec.Text.Lazy.parse Nix.Derivation.parseDerivation text of
+        Data.Attoparsec.Text.Lazy.Done "" drv -> pure drv
+        res -> error (show res)
+
+-------------------------------------------------------------------------------
+-- High level info: 'Material'
+-------------------------------------------------------------------------------
+
+-- | After walking the tree, the 'DrvInfo' are converted into a tree of 'Material'.
 data Material = Material
     { name :: Text
     , version :: Text
@@ -79,7 +105,7 @@ type Materials = Tree.Forest Material
 showMaterial :: Int -> Material -> Text
 showMaterial depth mat =
     mconcat
-        [Text.replicate depth " ", mat.name, " ", mat.version, " <", fromMaybe "unknown" mat.url, ">"]
+        [Text.replicate (depth * 2) " ", mat.name, " ", mat.version, " <", fromMaybe "unknown" mat.url, ">"]
 
 showMaterials :: Materials -> Text
 showMaterials mats = Text.unlines $ concatMap (showTree 0) mats
@@ -87,23 +113,6 @@ showMaterials mats = Text.unlines $ concatMap (showTree 0) mats
     showTree :: Int -> Tree.Tree Material -> [Text]
     showTree depth (Tree.Node mat childs) =
         showMaterial depth mat : concatMap (showTree $ depth + 1) childs
-
--- | A list of derivation name we don't want to cover.
--- note: this match any package, how to filter std build env?
-defaultIgnore :: Set Text
-defaultIgnore =
-    Set.fromList $ mconcat [stdenv, glib, gnu, libs, tools, comp, utils, build, rust, haskell]
-  where
-    stdenv = ["gcc", "xgcc", "clang", "llvm", "compiler-rt-libc", "linux-headers", "glibc", "coreutils", "binutils", "patch", "patchelf", "gmp"]
-    glib = ["glibc", "glibc-locales"]
-    gnu = ["gnutar", "gnumake", "gnugrep", "gnused", "gawk"]
-    libs = ["attr", "acl", "gmp-with-cxx", "gmp-with-cxx-stage4", "libidn2", "libmpc", "pcre", "pcre2", "mpfr", "pcre-light", "isl", "libunistring"]
-    comp = ["gzip", "zlib", "bzip2", "xz", "unzip"]
-    utils = ["findutils", "diffutils", "patchutils"]
-    tools = ["file", "rsync", "curl", "ed", "perl", "zstd", "pkg-config", "libffi", "jq", "openssl"]
-    build = ["bison", "bazel", "cmake", "ninja", "python3-minimal", "poetry-core", "sphinx"]
-    rust = ["cargo", "rust-docs", "rust-std", "cargo", "rustc", "crane-utils"]
-    haskell = ["ghc"]
 
 getBOM :: FilePath -> IO Materials
 getBOM root = do
@@ -123,14 +132,8 @@ getBOM root = do
         go :: FilePath -> IO Materials
         go fp = visitOnce fp do
             drv <- readDrv fp
-            let mDrvInfo = parseMaterial drv <|> parseSource drv
-            case mDrvInfo of
-                Just (MaterialInfo{name}) | name `Set.member` defaultIgnore -> pure mempty
-                _ -> goDrv drv mDrvInfo
-
-        goDrv drv mDrvInfo = do
             childs <- concat <$> traverse go (Map.keys drv.inputDrvs)
-            case mDrvInfo of
+            case parseDrvInfo drv of
                 Just (MaterialInfo{name, version, src}) -> do
                     url <- Map.lookup src <$> readIORef sources
                     pure [Tree.Node Material{name, version, url, src} childs]
@@ -143,17 +146,38 @@ getBOM root = do
                     pure childs
     go root
 
-readDrv :: FilePath -> IO Drv
-readDrv fp = do
-    text <- Data.Text.Lazy.IO.readFile fp
-    case Data.Attoparsec.Text.Lazy.parse Nix.Derivation.parseDerivation text of
-        Data.Attoparsec.Text.Lazy.Done "" drv -> pure drv
-        res -> error (show res)
+-------------------------------------------------------------------------------
+-- Filter implementation
+-------------------------------------------------------------------------------
 
-installableToDrvPath :: Text -> IO FilePath
-installableToDrvPath installable = do
-    Turtle.procs "nix" ["build", installable] mempty
-    Text.unpack <$> Turtle.single (readProc "nix" ["path-info", "--derivation", installable])
+filterMaterials :: (Material -> Bool) -> Materials -> Materials
+filterMaterials p = map (Tree.foldTree f)
+  where
+    f root childs = Tree.Node root (filter (not . p . Tree.rootLabel) childs)
+
+removeDefaults :: Material -> Bool
+removeDefaults mat = mat.name `Set.member` defaultIgnore
+
+-- | A list of derivation name we don't want to cover.
+-- note: this match any package, how to filter std build env?
+defaultIgnore :: Set Text
+defaultIgnore =
+    Set.fromList $ mconcat [stdenv, glib, gnu, libs, tools, comp, utils, build, rust, haskell]
+  where
+    stdenv = ["gcc", "xgcc", "clang", "llvm", "compiler-rt-libc", "linux-headers", "glibc", "coreutils", "binutils", "patch", "patchelf", "gmp"]
+    glib = ["glibc", "glibc-locales"]
+    gnu = ["gnutar", "gnumake", "gnugrep", "gnused", "gawk"]
+    libs = ["attr", "acl", "gmp-with-cxx", "gmp-with-cxx-stage4", "libidn2", "libmpc", "pcre", "pcre2", "mpfr", "pcre-light", "isl", "libunistring"]
+    comp = ["gzip", "zlib", "bzip2", "xz", "unzip"]
+    utils = ["findutils", "diffutils", "patchutils"]
+    tools = ["file", "rsync", "curl", "ed", "perl", "zstd", "pkg-config", "libffi", "jq", "openssl"]
+    build = ["bison", "bazel", "cmake", "ninja", "python3-minimal", "poetry-core", "sphinx"]
+    rust = ["cargo", "rust-docs", "rust-std", "cargo", "rustc", "crane-utils"]
+    haskell = ["ghc"]
+
+-------------------------------------------------------------------------------
+-- Diff implementation
+-------------------------------------------------------------------------------
 
 data Diff
     = Added Material
@@ -177,6 +201,10 @@ renderDiff = traverse_ go
     go = \case
         Added mat -> Text.putStrLn $ mconcat ["Added: ", showMaterial 0 mat]
         Modified smat dmat -> Text.putStrLn $ mconcat ["Modified ", smat.name, ": ", smat.version, " -> ", dmat.version]
+
+-------------------------------------------------------------------------------
+-- CLI entry point
+-------------------------------------------------------------------------------
 
 data Options = Options
     { action :: Action
@@ -202,18 +230,22 @@ parserInfo =
 readProc :: Text -> [Text] -> Turtle.Shell Text
 readProc cmd args = Turtle.lineToText <$> Turtle.inproc cmd args mempty
 
-getMats :: Text -> IO Materials
-getMats target = getBOM =<< installableToDrvPath target
+getMaterials :: Text -> IO Materials
+getMaterials target = filterMaterials removeDefaults <$> (getBOM =<< installableToDrvPath target)
+  where
+    installableToDrvPath installable = do
+        Turtle.procs "nix" ["build", installable] mempty
+        Text.unpack <$> Turtle.single (readProc "nix" ["path-info", "--derivation", installable])
 
 main :: IO ()
 main = do
     options <- O.execParser parserInfo
 
-    mats <- getMats options.target
+    mats <- getMaterials options.target
 
     case options.action of
         List -> Text.putStrLn $ showMaterials mats
         Count -> print (sum $ length <$> mats)
         Diff target -> do
-            omats <- getMats target
+            omats <- getMaterials target
             renderDiff (diffMaterials omats mats)
